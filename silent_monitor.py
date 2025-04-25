0c7c7a5dceb6dfcceb5f52d100403c30e18df471
Nico4O4 <nicopfin@bluewin.ch>
2025-04-25 18:35:34 +0200

    Update: Added comments

--- silent_monitor.py
+++ silent_monitor.py
@@ -2,18 +2,22 @@ import os
 import datetime
 import time
 
-for a in range(1,11):
+for a in range(1,256):
     
-    print("Scan gestartet am: " + str(datetime.datetime.now()))
-    print("-------------------------------------------------")
-    print(f"[~] Scanne IP: {a}")
-    ip = f"192.168.1.{a}"
+    print("Scan gestartet am: " + str(datetime.datetime.now())) #für den Scanzeitpunkt
+    print("-------------------------------------------------") # für die Trennung
+    print(f"[~] Scanne IP: {a}") #für die IP-Adresse
     
-    time.sleep(2)
-    response = os.system(f"ping -n 1  {ip}")
+    ip = f"123.456.7.{a}" # Hier die IP-Adresse anpassen
     
-    if response == 0:
-        print(f"[+] Gerät online {ip}")
+    time.sleep(0) # für die Pause zwischen den Scans (in Sekunden)
+    response = os.system(f"ping -n 1  {ip}") # für den Ping-Befehl (Windows linux ist "ping -c 1")
+    
+    if response == 0: # falls der Ping erfolgreich war
+        print(f"[+] Gerät online {ip}") # dann wird die IP-Adresse ausgegeben mt dem string "Gerät online"
     
     else: 
-        print(f"[-] Keine Antwort von: {ip} erhalten")
+        print(f"[-] Keine Antwort von: {ip} erhalten") # falls der Ping nicht erfolgreich war, wird die IP-Adresse ausgegeben mit dem string "Keine Antwort von: "
+    print("-------------------------------------------------") # für die Trennung
+
+    # coded by: Nico404
