87ae8bfe4ab6c43d33c662d3b1500b64a18c05de
Nico4O4 <nicopfin@bluewin.ch>
2025-05-08 17:15:22 +0200

    Update: Now you can do more scans much faster + exit the port scanner

--- portscanner.py
+++ portscanner.py
@@ -19,31 +19,47 @@ print("""
 \___/\____/\__,_/\___/\__,_/  /_.___/\__, (_)  /_/ |_/___/\____/\____/  /_/  \____/  /_/   
                                     /____/                                                """) # ASCII Art für den Portscanner
 
-ziel = input("Gib die IP-Adresse ein: ")# Hier die IP-Adresse anpassen
-print("-------------------------------------------------") # für die Trennung
+while True: # Endlosschleife für den Portscanner
+    print("-------------------------------------------------") # für die Trennung    
+    ziel = input("Gib die IP-Adresse ein: ")# Hier die IP-Adresse anpassen
+    print("-------------------------------------------------") # für die Trennung
 
-for port in range(0, 11):  # die anzahl der Ports die gescannt werden sollen, maximal 65535, hier 10 für den Test
+
+    for port in range(0, 11):  # die anzahl der Ports die gescannt werden sollen, maximal 65535, hier 10 für den Test
     
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)# AF_INET für IPv4 und SOCK_STREAM für TCP 
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)# AF_INET für IPv4 und SOCK_STREAM für TCP 
                             
-    s.settimeout(0.1)  # damit es nicht ewig hängt wenn es nichts findet
+        s.settimeout(0.1)  # damit es nicht ewig hängt wenn es nichts findet
     
-    ergebnis = s.connect_ex((ziel, port)) # connect_ex gibt 0 zurück, wenn der Port offen ist, sonst endet das Programm mit einer Fehlermeldung
+        ergebnis = s.connect_ex((ziel, port)) # connect_ex gibt 0 zurück, wenn der Port offen ist, sonst endet das Programm mit einer Fehlermeldung
     
     if ergebnis == 0: # 0 bedeutet, dass der Port offen ist weil connect_ex immer 0 zurückgibt wenn der Port offen ist
         print(f"Port {port} ist offen!") # wenn der Port offen ist, wird die Portnummer ausgegeben mit dem string "Port ist offen!" 
         found_ports_open.append(port) # wenn der Port offen ist, wird die Portnummer in die Liste "found_ports_open" hinzugefüg    
    
-    s.close() # am ende wird die Verbindung geschlossen
+        s.close() # am ende wird die Verbindung geschlossen
 
-print("-------------------------------------------------") # für die Trennung damit es schöner aussieht
+    print("-------------------------------------------------") # für die Trennung damit es schöner aussieht
 
-time = datetime.datetime.now() # für den Scanzeitpunkt am ende 
-print("Scan beendet am: " + str(time)) # printet den Scanzeitpunkt aus
+    time = datetime.datetime.now() # für den Scanzeitpunkt am ende 
+    print("Scan beendet am: " + str(time)) # printet den Scanzeitpunkt aus
 
 
-print("Offene gefundene Ports: " + str(found_ports_open)) # printet die offenen Ports aus
+    print("Offene gefundene Ports: " + str(found_ports_open)) # printet die offenen Ports aus
 
-print("-------------------------------------------------") # für die Trennung
+    print("-------------------------------------------------") # für die Trennung
+    
+    while True: # Endlosschleife für das Menü
+        menu = input("'Q' = Neue IP Adresse | 'E' = Beenden  ").strip().upper() # Menü für den Portscanner
+        
+        if menu == "E": # wenn man "E"eingibt wird das Programm beendet
+            print("beendet ")
+            exit()
+    
+        elif menu == "Q": # wenn man "Q" eingibt wird die Schleife neu gestartet    
+            continue  
+
+        else:
+            print("Ungültige Eingabe")
 
 # coded by: Nico404
