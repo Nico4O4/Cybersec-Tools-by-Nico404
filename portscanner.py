15fd289a5857e285697522bba98fd30885240abe
Nico4O4 <nicopfin@bluewin.ch>
2025-05-12 21:17:45 +0200

    Bugs fix and new comments

--- portscanner.py
+++ portscanner.py
@@ -19,25 +19,38 @@ print("""
 \___/\____/\__,_/\___/\__,_/  /_.___/\__, (_)  /_/ |_/___/\____/\____/  /_/  \____/  /_/   
                                     /____/                                                """) # ASCII Art für den Portscanner
 
-while True: # Endlosschleife für den Portscanner
+while True: # Schleife für den IP Adressen Check ob es eine gültige IP ist und nnicht abc123
     print("-------------------------------------------------") # für die Trennung    
     ziel = input("Gib die IP-Adresse ein: ")# Hier die IP-Adresse anpassen
     print("-------------------------------------------------") # für die Trennung
+    
+    try: 
+        socket.gethostbyname(ziel)
+    except socket.gaierror:
+        print("Ungültige Eingabe. Bitte Gültige IP-Adresse oder Domain angeben.")
+        continue # die Schleife startet neu weil keine gültige IP / Domain
 
+    # ---------------------------------------------------
+    # while True: # Endlosschleife für den Portscanner # BÖSE!!!! (und unnötig ^^)
+    # sobald 'ne gültige IP eingegeben wird, startet die für immer. (Danke für dein Kommentar und deine Hilfe :D)
+    # ---------------------------------------------------
 
     for port in range(0, 11):  # die anzahl der Ports die gescannt werden sollen, maximal 65535, hier 10 für den Test
     
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)# AF_INET für IPv4 und SOCK_STREAM für TCP 
-                            
-        s.settimeout(0.1)  # damit es nicht ewig hängt wenn es nichts findet
+
+        netz = socket.socket(socket.AF_INET, socket.SOCK_STREAM)# AF_INET für IPv4 (OSI Layer 3) und SOCK_STREAM für (OSI Layer 4) TCP / Variable "Netz" speichert die information zur Verbindung zum Beispiel bei welchen Port einen Verbindung möglich ist = offen
+                        
+        netz.settimeout(0.1)  # damit es nicht ewig hängt wenn es nichts findet oder es sonst einen fehler gibt
     
-        ergebnis = s.connect_ex((ziel, port)) # connect_ex gibt 0 zurück, wenn der Port offen ist, sonst endet das Programm mit einer Fehlermeldung
+        ergebnis = netz.connect_ex((ziel, port)) # connect_ex gibt 0 zurück, wenn der Port offen ist, sonst endet das Programm mit einer Fehlermeldung / connect_ex macht einen Verbindungs versuch zu einem TCP Port -> er testet ob eine verbindung möglich ist  
+        # Ergebnis speichert die Rückgabe von netz.connect_ex(), und das if fragt dann, ob ergebnis == 0 (also die Verbindung erfolgreich war) und entscheidet daraufhin, was im if-Block passiert.
     
+
     if ergebnis == 0: # 0 bedeutet, dass der Port offen ist weil connect_ex immer 0 zurückgibt wenn der Port offen ist
         print(f"Port {port} ist offen!") # wenn der Port offen ist, wird die Portnummer ausgegeben mit dem string "Port ist offen!" 
         found_ports_open.append(port) # wenn der Port offen ist, wird die Portnummer in die Liste "found_ports_open" hinzugefüg    
-   
-        s.close() # am ende wird die Verbindung geschlossen
+
+        netz.close() # am ende wird die Verbindung geschlossen 
 
     print("-------------------------------------------------") # für die Trennung damit es schöner aussieht
 
@@ -48,18 +61,18 @@ while True: # Endlosschleife für den Portscanner
     print("Offene gefundene Ports: " + str(found_ports_open)) # printet die offenen Ports aus
 
     print("-------------------------------------------------") # für die Trennung
+
+    while True: # Endlosschleife für das End-Menü
+        menu = input("'Q' = Neue IP Adresse | 'E' = Beenden  ").strip().upper() # Menü für den Portscanner / # strip upper dient dazu das man auch kleine Buchstaben eingeben kann wie q oder e
     
-    while True: # Endlosschleife für das Menü
-        menu = input("'Q' = Neue IP Adresse | 'E' = Beenden  ").strip().upper() # Menü für den Portscanner
-        
         if menu == "E": # wenn man "E"eingibt wird das Programm beendet
             print("beendet ")
             exit()
-    
-        elif menu == "Q": # wenn man "Q" eingibt wird die Schleife neu gestartet    
-            continue  
+
+        if menu == "Q": # wenn man "Q" eingibt kehrt man zur 1. Schleife zurück  (1 von 3 Schleifen)
+            break
 
         else:
             print("Ungültige Eingabe")
 
-# coded by: Nico404
+# coded by: Nico40
